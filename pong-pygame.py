7a711760fd71345f2aca15d075b2149f551bbd71
Shannon Brookshire <swbrookshire@gmail.com>
2022-08-26 21:05:40 -0500

    created variables for the objects on the screen

--- pong-pygame.py
+++ pong-pygame.py
@@ -9,6 +9,10 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 -15,30,30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70,10,140)
+opponent = pygame.Rect(10, screen_height/2 - 70,10,140)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
