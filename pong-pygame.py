91a67fc7bc08e8067afbab7f482a968bea4c8845
Shannon Brookshire <swbrookshire@gmail.com>
2022-08-26 20:47:42 -0500

    Corrected a typo

--- pong-pygame.py
+++ pong-pygame.py
@@ -1,1 +1,19 @@
-import pygame
+from webbrowser import get
+import pygame, sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Pong')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+
+    pygame.display.flip()
+    clock.tick(60)
