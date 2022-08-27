43ea968dc9d252eb0ebbf3aed365703fd2f74cfa
Shannon Brookshire <swbrookshire@gmail.com>
2022-08-26 21:14:25 -0500

    Drew objects to the screen surface

--- pong-pygame.py
+++ pong-pygame.py
@@ -13,11 +13,21 @@ ball = pygame.Rect(screen_width/2 - 15, screen_height/2 -15,30,30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70,10,140)
 opponent = pygame.Rect(10, screen_height/2 - 70,10,140)
 
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
+
+
+    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,opponent)
+    pygame.draw.ellipse(screen,light_grey,ball)
+    
 
     pygame.display.flip()
     clock.tick(60)
