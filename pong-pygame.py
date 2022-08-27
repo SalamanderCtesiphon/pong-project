254d5bc026cc3f57300c179e1f3dd9f177206977
Shannon Brookshire <swbrookshire@gmail.com>
2022-08-27 14:10:37 -0500

    added limits to the opponent to prevent leaving the screen

--- pong-pygame.py
+++ pong-pygame.py
@@ -12,17 +12,26 @@ def ball_animation():
     if ball.left <= 0 or ball.right >= screen_width:
         ball_speed_x *= -1
 
-    if ball .colliderect(player) or ball.colliderect(opponent):
+    if ball .colliderect(opponent) or ball.colliderect(opponent):
         ball_speed_x *= -1
 
 def player_animation():
-    player.y += player_speed
+    opponent.y += player_speed
 
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= screen_height:
-        player.bottom = screen_height
-             
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+def opponent_animation():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height            
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -33,7 +42,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 -15,30,30)
-player = pygame.Rect(screen_width - 20, screen_height/2 - 70,10,140)
+opponent = pygame.Rect(screen_width - 20, screen_height/2 - 70,10,140)
 opponent = pygame.Rect(10, screen_height/2 - 70,10,140)
 
 bg_color = pygame.Color('grey12')
@@ -42,6 +51,7 @@ light_grey = (200,200,200)
 ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
+opponent_speed = 0
 
 while True:
     for event in pygame.event.get():
@@ -61,16 +71,12 @@ while True:
 
     player_animation()
     ball_animation()
-    if opponent.top < ball.y:
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.y:
-        opponent.bottom -= opponent_speed
-        
+    opponent_animation()    
     
 
 
     screen.fill(bg_color)
-    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,opponent)
     pygame.draw.rect(screen,light_grey,opponent)
     pygame.draw.ellipse(screen,light_grey,ball)
     pygame.draw.aaline(screen, light_grey,
