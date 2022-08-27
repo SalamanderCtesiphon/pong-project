fa1b6073186ba3134655f369f15b88c263239eaf
Shannon Brookshire <swbrookshire@gmail.com>
2022-08-26 20:49:15 -0500

    Adjusted the screen size

--- pong-pygame.py
+++ pong-pygame.py
@@ -4,7 +4,7 @@ import pygame, sys
 pygame.init()
 clock = pygame.time.Clock()
 
-screen_width = 800
+screen_width = 1000
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
